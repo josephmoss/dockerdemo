85b8b673aeef5c15a132042d5dfa1a993a7a0697
Joe Moss <joseph.moss@gmail.com>
2019-05-18 18:40:49 -0400

    Add project files.

---
+++ DockerDemo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DockerDemo.Models;
+
+namespace DockerDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello world!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return NotFound();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DockerDemoTests/UnitTest.cs
@@ -0,0 +1,25 @@
+using DockerDemo.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace DockerDemoTests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void Index_ReturnsAViewResult_WithAHelloWorldMessage()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var viewBagMessage = Assert.IsAssignableFrom<string>(viewResult.ViewData["Message"]);
+
+            Assert.Equal("Hello world!", viewBagMessage);
+        }
+    }
+}
