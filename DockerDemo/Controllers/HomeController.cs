6e81ab6c19609ad9b32f5a4467aa707b9e725973
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-22 19:50:12 +0000

    Deploy josephmoss/dockerdemo to github.com/josephmoss/dockerdemo.git:master

--- DockerDemo/Controllers/HomeController.cs
+++ DockerDemo/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace DockerDemo.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.Message = "Hello world! 5/19/2019";
+            ViewBag.Message = "Hello world! 5/22/2019";
             return View();
         }
 
